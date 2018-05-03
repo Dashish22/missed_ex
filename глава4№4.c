4d228384039d6e3e7e0521c2f21e7aac54464024
Dashish22 <32478557+Dashish22@users.noreply.github.com>
2018-05-03 14:11:43 +0300

    Update глава4№4.c

--- глава4№4.c
+++ глава4№4.c
@@ -4,6 +4,6 @@ int main(void)
         float F, C;
         F = 27.0;
         C = (F - 32)/1.8;
-    printf("27 градусов по Фаренгейту = %.2f градусов по Цельсию\n", C);
+    printf("27 C = %.2f F\n", C);
 	return 0;
 }
