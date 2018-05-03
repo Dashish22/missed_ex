c8c68510358b57a9e9a1d8b202e910ad612a7930
Dashish22 <32478557+Dashish22@users.noreply.github.com>
2018-05-03 14:10:51 +0300

    Add files via upload
    st.k.

---
+++ глава4№4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{ 
+        float F, C;
+        F = 27.0;
+        C = (F - 32)/1.8;
+    printf("27 градусов по Фаренгейту = %.2f градусов по Цельсию\n", C);
+	return 0;
+}
